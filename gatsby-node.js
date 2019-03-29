aab5ac1de3167acd84348dc9c3d5791e4d4cb94f
勤硕 <qinshuo.lqs@alibaba-inc.com>
2019-03-29 16:29:58 +0800

    Fix: won't genetare hidden post

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,6 +9,7 @@ exports.createPages = ({ graphql, actions }) => {
     `
       {
         allMarkdownRemark(
+          filter: { fields: { slug: { regex: "/^(?!/_)\\\\.*/" } } }
           sort: { fields: [frontmatter___date], order: DESC }
           limit: 1000
         ) {
