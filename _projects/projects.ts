74935ab439d92aed054cf0754c14252045b26d78
Allen <allen@ultiverse.io>
2023-07-05 15:02:42 +0800

    projects

---
+++ _projects/projects.ts
@@ -0,0 +1,32 @@
+export const projects = [
+  {
+    name: "slate-table",
+    description: "A editable table plugin for Slate.js (> v0.5)",
+    url: "https://slate-table.vercel.app/",
+    github: "https://github.com/lqs469/slate-table"
+  },
+  {
+    name: "micro-serverless",
+    description: "A simple micro serverless service based on Node.js VM",
+    url: "https://github.com/lqs469/micro-serverless",
+    github: "https://github.com/lqs469/micro-serverless"
+  },
+  {
+    name: "Christmas-hat",
+    description: "Put on Sant cap based on face detection",
+    url: "https://santahat.now.sh",
+    github: "https://github.com/lqs469/X-mas-hat"
+  },
+  {
+    name: "jump-assistant",
+    description: "An automatic simulator for a boring wechat mini-game",
+    url: "https://github.com/lqs469/jump",
+    github: "https://github.com/lqs469/jump"
+  },
+  {
+    name: "Confluence-export",
+    description: "Export document from confluence with nice style",
+    url: "https://github.com/lqs469/confluence-export",
+    github: "https://github.com/lqs469/confluence-export"
+  }
+];
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -5,9 +5,17 @@ import Header from "../components/header";
 import Layout from "../components/layout";
 import Head from "next/head";
 import { Nav } from "../components/nav";
+import { projects } from "../_projects/projects";
+import githubIcon from "../public/img/github.svg";
+import { useCallback } from "react";
 
 type Props = {
-  projects: any[];
+  projects: {
+    name: string;
+    description: string;
+    url: string;
+    github: string;
+  }[];
 };
 
 const title = "Projects";
@@ -23,22 +31,45 @@ export default function Projects({ projects }: Props) {
             {/* <meta property="og:image" content={post.ogImage.url} /> */}
           </Head>
           <Nav />
-          <section className=""></section>
+          <section className="">
+            {projects.map((project, index) => (
+              <li
+                key={project.name}
+                className="flex justify-between items-center leading-10 border-b"
+              >
+                <a
+                  className="font-semibold no-underline"
+                  href={project.url}
+                  target="_blank"
+                >
+                  {project.name}
+                </a>
+                <p className="opacity-70 inline-flex items-center">
+                  <a
+                    className="no-underline"
+                    href={project.url}
+                    target="_blank"
+                  >
+                    {project.description}
+                  </a>
+                  <a className="ml-2" href={project.github} target="_blank">
+                    <img
+                      className="h-4"
+                      src={"/assets/github.svg"}
+                      alt={project.github}
+                    />
+                  </a>
+                </p>
+              </li>
+            ))}
+          </section>
         </article>
       </Layout>
     </Container>
   );
 }
 
-type Params = {
-  params: {
-    slug: string;
-  };
-};
-
-export async function getStaticProps({ params }: Params) {
-  const projects = [];
-
+export async function getStaticProps() {
   return {
     props: {
       projects
